a9216860b85222cacd2a2f102a6f4a5853ae65c9
MOHAMMED RIYAZ S <mohammedriyazs0506@gmail.com>
2026-02-15 03:38:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
 MYSQL_CONFIG = {
-    "host": os.environ.get("MYSQL_HOST"),
-    "user": os.environ.get("MYSQL_USER"),
-    "password": os.environ.get("MYSQL_PASSWORD"),
-    "database": os.environ.get("MYSQL_DATABASE"),
-    "port": int(os.environ.get("MYSQL_PORT", 3306)),
+    "host": os.environ.get("DB_HOST"),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "database": os.environ.get("DB_NAME"),
+    "port": int(os.environ.get("DB_PORT", 3306)),
     "ssl_disabled": False
 }
+
