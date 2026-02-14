415397504fc5d561848d1f8afc9626f156f7fbeb
MOHAMMED RIYAZ S <mohammedriyazs0506@gmail.com>
2026-02-15 03:15:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_socketio import SocketIO
 import mysql.connector
